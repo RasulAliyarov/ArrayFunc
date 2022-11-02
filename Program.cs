7e2e533653a80d85c452ad4caf7fd229fc5dc8a5
RasulAliyarov <rasulaliyarov2002@gmail.com>
2022-11-03 01:39:46 +0400

    add c# file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int[] numbers = new int[] {1,2,3,4};
+
+for (int i =0; i<numbers.Length; i++) //O(n+1)
+{
+    Console.Write(numbers[i]+", ");//O(n)
+}
+
+Console.WriteLine(" ");
+
+for (int i = 0; i < numbers.Length; i++) //O(n+1)
+{
+    Console.Write(numbers[i]+1 + ", ");//O(n)
+}
